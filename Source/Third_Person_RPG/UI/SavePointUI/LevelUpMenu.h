1cee3e84852fecfde96d0e5309abd36938a5c45d
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2025-09-09 22:13:20 +0900

    레벨업 시스템 구현 중

---
+++ Source/Third_Person_RPG/UI/SavePointUI/LevelUpMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LevelUpMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRD_PERSON_RPG_API ULevelUpMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
