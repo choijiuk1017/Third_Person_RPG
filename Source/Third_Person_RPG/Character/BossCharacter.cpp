fe5d7e9d05001ce0963817a0839ebf009896bc57
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2025-09-26 03:27:29 +0900

    튜토리얼 보스 제작 중

---
+++ Source/Third_Person_RPG/Character/BossCharacter.cpp
@@ -0,0 +1,285 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Third_Person_RPG/Character/BossCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Third_Person_RPG/Character/EnemyAIController.h"
+#include "Third_Person_RPG/Character/PlayerCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Engine/EngineTypes.h"
+#include "Engine/OverlapResult.h"
+#include "Components/ProgressBar.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+#define CHANNEL_ACTION ECollisionChannel::ECC_GameTraceChannel2
+// Sets default values
+ABossCharacter::ABossCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	AIControllerClass = AEnemyAIController::StaticClass();
+
+	GetCapsuleComponent()->SetCollisionResponseToChannel(CHANNEL_ACTION, ECR_Overlap);
+	GetCapsuleComponent()->SetGenerateOverlapEvents(true);
+
+}
+
+// Called when the game starts or when spawned
+void ABossCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+	{
+		if (HPBarWidgetClass)
+		{
+			HPBarWidget = CreateWidget<UHPBar>(PC, HPBarWidgetClass);
+			if (HPBarWidget)
+			{
+				HPBarWidget->AddToViewport();
+			}
+		}
+	}
+}
+
+// Called every frame
+void ABossCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABossCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ABossCharacter::BaseAttackCheck()
+{
+	TArray<FOverlapResult> OverlapResults;
+
+	// 충돌 탐지를 위한 시작 지점
+	FVector Start = GetActorLocation() + (GetActorForwardVector() * GetCapsuleComponent()->GetScaledCapsuleRadius());
+
+	// 충돌 탐지 끝 지점
+	FVector End = Start + (GetActorForwardVector() * AttackRange);
+
+	// 오버랩 중심점 (Capsule처럼 보이게)
+	FVector CapsuleOrigin = Start + (End - Start) * 0.5f;
+	FQuat CapsuleRotation = FRotationMatrix::MakeFromZ(GetActorForwardVector()).ToQuat();
+
+	// 오버랩 영역 설정
+	FCollisionShape CollisionShape = FCollisionShape::MakeCapsule(AttackRange, AttackRange * 0.5f);  // Radius, HalfHeight
+
+	// 쿼리 파라미터
+	FCollisionQueryParams Params(SCENE_QUERY_STAT(AttackOverlap), false, this);
+
+	// 오버랩 실행
+	bool bHasHit = GetWorld()->OverlapMultiByChannel(
+		OverlapResults,
+		CapsuleOrigin,
+		CapsuleRotation,
+		CHANNEL_ACTION,
+		CollisionShape,
+		Params
+	);
+
+	// 피격 판정 처리
+	if (bHasHit)
+	{
+		TSet<APlayerCharacter*> DamagedPlayers;
+
+		for (const FOverlapResult& Result : OverlapResults)
+		{
+			if (APlayerCharacter* Player = Cast<APlayerCharacter>(Result.GetActor()))
+			{
+				if (!DamagedPlayers.Contains(Player))
+				{
+					DamagedPlayers.Add(Player);
+					UE_LOG(LogTemp, Warning, TEXT("Player Damaged via Overlap"));
+
+					Player->TakeDamage(BossStats.AttackPower);
+				}
+			}
+		}
+	}
+
+	// 디버그용 캡슐 시각화
+	FColor DrawColor = bHasHit ? FColor::Green : FColor::Red;
+	DrawDebugCapsule(GetWorld(), CapsuleOrigin, AttackRange * 0.5f, AttackRange, CapsuleRotation, DrawColor, false, 3.0f);
+}
+
+void ABossCharacter::PlayAttackMontageByIndex(int32 Index)
+{
+	if (AttackMontages.IsValidIndex(Index) && GetMesh() && GetMesh()->GetAnimInstance())
+	{
+		UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+		AnimInstance->Montage_Play(AttackMontages[Index]);
+
+		// 종료 콜백
+		FOnMontageEnded EndDelegate;
+		EndDelegate.BindUObject(this, &ABossCharacter::OnAttackMontageEnded);
+		AnimInstance->Montage_SetEndDelegate(EndDelegate, AttackMontages[Index]);
+	}
+}
+
+void ABossCharacter::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+
+	CurrentAttackStep++;
+
+	if (CurrentAttackStep >= AttackMontages.Num())
+	{
+		CurrentAttackStep = 0;
+		return;
+	}
+}
+
+void ABossCharacter::PlayHitReactMontage()
+{
+	if (HitReactMontage && GetMesh() && GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_Play(HitReactMontage);
+	}
+}
+
+void ABossCharacter::RegisterAttacker(AActor* Attacker)
+{
+	if (!Attacker) return;
+	if (bIsDead) return;
+
+	if (APlayerCharacter* PC = Cast<APlayerCharacter>(Attacker))
+	{
+		LastAttacker = PC;
+	}
+}
+
+void ABossCharacter::GrantCurrencyToKiller()
+{
+	if (bRewardGranted) return;
+	if (!LastAttacker.IsValid()) return;
+	UE_LOG(LogTemp, Warning, TEXT("LastAttacker Reward"));
+	LastAttacker->AddCurrency(CurrencyReward);
+	bRewardGranted = true;
+}
+
+
+void ABossCharacter::TakeDamage(int32 DamageAmount)
+{
+	int32 Defense = BossStats.Defense;
+
+	float DamageMultiplier = 100.f / (100.f + static_cast<float>(Defense));
+	float FinalDamage = DamageAmount * DamageMultiplier;
+
+	BossStats.CurrentHP -= FinalDamage;
+
+
+	if (BossStats.CurrentHP <= 0)
+	{
+
+		if (AEnemyAIController* AIController = Cast<AEnemyAIController>(GetController()))
+		{
+			AIController->PauseAI();
+		}
+
+		// 사망 처리
+		BossStats.CurrentHP = 0;
+		bIsDead = true;
+
+		GrantCurrencyToKiller();
+
+		if (HPBarWidget)
+		{
+			GetWorld()->GetTimerManager().ClearTimer(HideHPBarTimerHandle);
+			HPBarWidget->RemoveFromParent();
+			HPBarWidget = nullptr;
+		}
+
+		AAIController* AIController = Cast<AAIController>(GetController());
+		if (AIController)
+		{
+			AIController->StopMovement();
+			AIController->UnPossess();
+		}
+
+		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+		USkeletalMeshComponent* MeshComponent = GetMesh();
+		MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		MeshComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+
+
+		UE_LOG(LogTemp, Error, TEXT("Monster Dead"));
+
+		return;
+	}
+
+	if (UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance())
+	{
+		if (HitReactMontage)
+		{
+			AnimInstance->Montage_Play(HitReactMontage);
+
+			if (AEnemyAIController* EnemyAI = Cast<AEnemyAIController>(GetController()))
+			{
+				EnemyAI->PauseAI();
+			}
+
+			GetCharacterMovement()->DisableMovement();
+
+			GetWorld()->GetTimerManager().SetTimer(
+				HitReactTimerHandle,
+				this,
+				&ABossCharacter::EndHitReact,
+				HitReactDuration,
+				false
+			);
+		}
+	}
+
+
+
+	if (HPBarWidget)
+	{
+		float Percent = static_cast<float>(BossStats.CurrentHP) / static_cast<float>(BossStats.MaxHP);
+		HPBarWidget->SetHPBarPercent(Percent);
+
+		HPBarWidget->SetVisibility(ESlateVisibility::Visible);
+
+		GetWorld()->GetTimerManager().ClearTimer(HideHPBarTimerHandle);
+		GetWorld()->GetTimerManager().SetTimer(
+			HideHPBarTimerHandle,
+			[this]()
+			{
+				if (HPBarWidget && !bIsDead)
+				{
+					HPBarWidget->SetVisibility(ESlateVisibility::Hidden);
+				}
+			},
+			1.0f,
+			false
+		);
+	}
+}
+
+void ABossCharacter::EndHitReact()
+{
+	if (!bIsDead)
+	{
+		GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+
+		if (AEnemyAIController* EnemyAI = Cast<AEnemyAIController>(GetController()))
+		{
+			EnemyAI->ResumeAI();
+		}
+	}
+}
---
+++ Source/Third_Person_RPG/Character/BossCharacter.h
@@ -0,0 +1,126 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Components/WidgetComponent.h"
+#include "Third_Person_RPG/Interface/AnimationAttackInterface.h"
+#include "Third_Person_RPG/UI/HPBar.h"
+#include "Third_Person_RPG/Character/PlayerCharacter.h"
+#include "BossCharacter.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBossStats
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	int32 MaxHP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	int32 CurrentHP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	int32 AttackPower;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	int32 Defense;
+};
+
+UCLASS()
+class THIRD_PERSON_RPG_API ABossCharacter : public ACharacter, public IAnimationAttackInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABossCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void BaseAttackCheck() override;
+	virtual void SkillAttackCheck() override {};
+	virtual void EnableWeaponHitBox() override {};
+	virtual void DisableWeaponHitBox() override {};
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI")
+	class UBehaviorTree* BehaviorTreeAsset;
+
+	void TakeDamage(int32 DamageAmount);
+
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	TArray<UAnimMontage*> AttackMontages;
+
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	TArray<UAnimMontage*> SkillMontages;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat|Attack")
+	int32 CurrentAttackStep = 0;
+
+
+	UFUNCTION(BlueprintCallable, Category = "Combat|Attack")
+	void PlayAttackMontageByIndex(int32 Index);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Stats")
+	FBossStats BossStats;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	bool bIsDead;
+
+	UPROPERTY()
+	UHPBar* HPBarWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UHPBar> HPBarWidgetClass;
+
+	UFUNCTION(BlueprintCallable, Category = "Reward")
+	void RegisterAttacker(AActor* Attacker);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HitReact")
+	float HitReactDuration = 0.5f;
+
+	void PlayHitReactMontage();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = Animation)
+	TObjectPtr<class UAnimMontage> HitReactMontage;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	float AttackRange;
+
+private:
+
+	FTimerHandle HitReactTimerHandle;
+
+	FTimerHandle HideHPBarTimerHandle;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Reward", meta = (AllowPrivateAccess = "true"))
+	int32 CurrencyReward = 50;
+
+	// 마지막으로 이 적에게 피해를 준 플레이어
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reward", meta = (AllowPrivateAccess = "true"))
+	TWeakObjectPtr<APlayerCharacter> LastAttacker;
+
+	// 보상 중복 지급 방지
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Reward", meta = (AllowPrivateAccess = "true"))
+	bool bRewardGranted = false;
+
+	void GrantCurrencyToKiller();
+
+	UFUNCTION()
+	void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+
+	UFUNCTION()
+	void EndHitReact();
+};
